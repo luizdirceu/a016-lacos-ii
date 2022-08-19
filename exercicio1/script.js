068e6268911cc66132bf453004fadccd873e0be7
Luiz Dirceu <juninholdmarques@gmail.com>
2022-08-18 21:09:48 -0300

    atividade

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -5,3 +5,15 @@ const array = [
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+
+if (array.length === 5) {
+    for (let contador1 = 0; contador1 < array.length; contador1++) {
+        let gols = ` jogador ${contador1 + 1}: `
+        for (let contador2 = 0; contador2 < array[contador1].length; contador2++) {
+            gols += `${array[contador1][contador2]} `
+        }
+console.log (gols)
+    }
+
+}
+
---
+++ exercicio3/script.js
@@ -0,0 +1,5 @@
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"]
+let valor = 1
+
+for(let i of maioresPaises){ 
+console.log (`${valor++} - ${[i]}`)} 
